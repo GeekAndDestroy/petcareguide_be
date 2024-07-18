7b1bbf635fe9d6435c2b2f69fa3d9bc898a26205
Jeff Chebul <jmchebul@jmchebul.com>
2024-07-18 17:22:44 -0500

    reverted to online DB

--- petcareguide/settings.py
+++ petcareguide/settings.py
@@ -99,30 +99,30 @@ WSGI_APPLICATION = "petcareguide.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'petcareguide',
-#         'USER': 'admin',
-#         # 'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-#         # 'HOST': os.getenv('DATABASE_URL'),
-#         # 'PASSWORD': 'SaveMe36!',
-#         # 'HOST': 'localhost',
-#         'PASSWORD': 'nifsym-beBqyp-9cunbu',
-#         'HOST': 'database-1.c5o4y6qe66md.us-east-2.rds.amazonaws.com',
-#         'PORT': '3306'
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'petcareguide',
-        'USER': 'postgres',
-        'PASSWORD': 'SaveMe36!',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'USER': 'admin',
+        # 'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        # 'HOST': os.getenv('DATABASE_URL'),
+        # 'PASSWORD': 'SaveMe36!',
+        # 'HOST': 'localhost',
+        'PASSWORD': 'nifsym-beBqyp-9cunbu',
+        'HOST': 'database-1.c5o4y6qe66md.us-east-2.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'petcareguide',
+#         'USER': 'postgres',
+#         'PASSWORD': 'SaveMe36!',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
